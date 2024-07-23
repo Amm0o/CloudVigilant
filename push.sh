1b962ed419f6d70e9af7b6b3bba8a896242d66ef
anoliveiraMLap <anoliveira@anoliveira>
2024-07-23 11:15:53 +0100

    Adding domain

--- push.sh
+++ push.sh
@@ -8,4 +8,4 @@ read commitMessage
 # commit and push
 git add -A
 git commit -a -m "$commitMessage"
-git push
+git push https://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@github.com/Amm0o/Heimdall.git
